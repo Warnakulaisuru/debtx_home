199745ef17d2a7451916fcddb5910466d6c9e26c
Warnakulaisuru <warnakulaisuru@gmail.com>
2025-06-26 12:48:20 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { MsalProvider } from '@azure/msal-react';
-import { PublicClientApplication } from '@azure/msal-browser';
-import { msalConfig } from './msalConfig';
-import './index.css';
-
-const msalInstance = new PublicClientApplication(msalConfig);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </MsalProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
